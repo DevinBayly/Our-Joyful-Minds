606eb2f1f0b5ede60c302be05263d849c9d998bb
Devin Bayly <baylyd@email.arizona.edu>
2021-12-29 01:21:32 +0000

    making edits for Ms. Blue email

--- reporter_search/autofill_notes.py
+++ reporter_search/autofill_notes.py
@@ -2,8 +2,14 @@ import yaml
 res = yaml.load(open("notes.yml").read(),Loader=yaml.Loader)
 for name in res["contacts"]:
     if res.get(name,-1) ==-1:
-        res[name]=dict(notes=[],email=None)
+        res[name]=dict(notes=[],
+        email=None,
+        recipient_name=None,
+        appeal_hook=None)
 
 with open("notes.yml","w") as phile:
     phile.write(yaml.dump(res))
 
+## things the class does
+# expands new entries
+# writes out templated versions of the article for each of the 
